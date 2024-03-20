1fef5523e6dcb53102e42caffa42c3771f597f08
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-03-21 01:52:00 +0600

    updated

--- app/api/note/DatabaseComponent.tsx
+++ app/api/note/DatabaseComponent.tsx
@@ -1,17 +1,4 @@
-/*import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
-import { text,integer,sqliteTable } from 'drizzle-orm/sqlite-core';
 
-const sqlite = new Database('sqlite.db'); 
-const db: BetterSQLite3Database = drizzle(sqlite);
-const notesTable = sqliteTable("notes", {
-  note_id: text('note_id'),
-  title: text('title'),
-  content: text('content'),
-  email: text('email')
-});
-
-export  { db,notesTable };*/
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from "@vercel/postgres";
 import {
--- app/api/note/route.tsx
+++ app/api/note/route.tsx
@@ -14,7 +14,7 @@ export async function GET(req, res) {
   const session = await getServerSession(authOptions)
   if (session){
     
-    const notes=db.select().from(notesTable).where(like(notesTable.email,session.user?.email)).all()
+    const notes=db.select().from(notesTable).where(like(notesTable.email,session.user?.email))
     
     // Return the items as a JSON response with status 200
     return new Response(JSON.stringify(notes), {
