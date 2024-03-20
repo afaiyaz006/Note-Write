669a72a8e933ebfbc2cec390e49c285b948763c2
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-03-21 01:40:00 +0600

    switched from sqllite to postgres

--- app/api/note/DatabaseComponent.tsx
+++ app/api/note/DatabaseComponent.tsx
@@ -1,4 +1,4 @@
-import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
+/*import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import { text,integer,sqliteTable } from 'drizzle-orm/sqlite-core';
 
@@ -11,4 +11,28 @@ const notesTable = sqliteTable("notes", {
   email: text('email')
 });
 
+export  { db,notesTable };*/
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { sql } from "@vercel/postgres";
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from 'drizzle-orm/pg-core';
+
+// Use this object to send drizzle queries to your DB
+const db = drizzle(sql);
+// Create a pgTable that maps to a table in your DB
+const notesTable = pgTable(
+  "notes", {
+    note_id: text('note_id'),
+    title: text('title'),
+    content: text('content'),
+    email: text('email')
+  }
+
+);
 export  { db,notesTable };
+
