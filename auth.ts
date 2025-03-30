faf16caaf626c3346956b1d178c70df1a32cdfdf
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2025-03-30 17:53:02 +0600

    added github as login provider

--- auth.ts
+++ auth.ts
@@ -16,5 +16,11 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
   trustedOrigins: ["*"],
 });
